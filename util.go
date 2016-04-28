8fcf43518f6942d71a9df1016bef758e441513a3
Blixt <me@blixt.nyc>
2016-04-28 12:32:38 -0400

    Add UUID type for creating unique ids

--- util.go
+++ util.go
@@ -1,8 +1,10 @@
 package avs
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
+	"io"
 	"mime"
 	"mime/multipart"
 	"net/http"
@@ -13,6 +15,38 @@ import (
 )
 
 var quoteEscaper = strings.NewReplacer("\\", "\\\\", `"`, "\\\"")
+
+// UUID holds a 16 byte unique identifier.
+type UUID []byte
+
+// String returns a string representation of the UUID.
+func (uuid UUID) String() string {
+	if len(uuid) != 16 {
+		return ""
+	}
+	b := []byte(uuid)
+	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+}
+
+// NewUUID returns a randomly generated UUID.
+func NewUUID() (UUID, error) {
+	b := make([]byte, 16)
+	n, err := io.ReadFull(rand.Reader, b)
+	if n != len(b) || err != nil {
+		return nil, err
+	}
+	b[8] = b[8]&^0xc0 | 0x80
+	b[6] = b[6]&^0xf0 | 0x40
+	return UUID(b), nil
+}
+
+func GetUUIDString() string {
+	uuid, err := NewUUID()
+	if err != nil {
+		panic("GetUUIDString: unable to create UUID")
+	}
+	return uuid.String()
+}
 
 func newMultipartReaderFromResponse(resp *http.Response) (*multipart2.Reader, error) {
 	// Work around bug in Amazon's downchannel server.
